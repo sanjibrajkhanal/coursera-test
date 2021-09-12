9a2ba354806f22fd15af3bcd7e27feeeb089fbf1
Sanjib Raj Khanal <sanjibrajkhanal@gmail.com>
2021-09-12 14:29:21 +0900

    anything

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,8 +83,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  function(reponseText){
-    buildAndShowHomeHTML(responseText);
+  function(request){
+    buildAndShowHomeHTML(request);
   },
   // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
